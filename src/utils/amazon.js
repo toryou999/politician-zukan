7ef085e210a0078dc8e814d6e1db6e0cc998a01e
toryou999 <ride3273560@gmail.com>
2026-02-05 02:28:50 +0900

    Config: AmazonアソシエイトID設定

--- src/utils/amazon.js
+++ src/utils/amazon.js
@@ -1,6 +1,6 @@
 // Amazonアソシエイト設定
 // ユーザー自身のタグに変更してください（今は仮のIDを入れています）
-export const AMAZON_ASSOCIATE_TAG = 'politician-zukan-22';
+export const AMAZON_ASSOCIATE_TAG = 'bassarijpgo-22';
 
 export const getAmazonProductUrl = (asin) => {
     return `https://www.amazon.co.jp/dp/${asin}?tag=${AMAZON_ASSOCIATE_TAG}&linkCode=ogi&th=1&psc=1`;
