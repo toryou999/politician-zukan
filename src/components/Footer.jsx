625376e76474d9021e3d48473472a987df681aff
toryou999 <ride3273560@gmail.com>
2026-02-06 01:10:44 +0900

    Update: v1.1.0 (Add version info to footer)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,7 @@ function Footer() {
                     <Link to="/privacy">プライバシーポリシー</Link>
                 </div>
                 <p className="copyright">© 2026 政治家図鑑 All Rights Reserved.</p>
+                <p style={{ textAlign: 'center', fontSize: '10px', color: '#ccc', marginTop: '5px' }}>v1.1.0</p>
             </div>
         </footer>
     );
